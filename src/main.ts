5c652cdf9dbe97da885833b4a25eee7997e7ec97
tutn <azjustin3@gmail.com>
2023-08-14 01:02:13 +0700

    Enable cors and cookies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+  app.use(cookieParser())
   await app.listen(configService.get<string>('http.port'));
 }
 bootstrap();
