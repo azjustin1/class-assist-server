1ee6c7b9c454160a69b4b5772ca8acb0578d112e
tutn <azjustin3@gmail.com>
2023-08-17 00:28:37 +0700

    Update app config

--- src/app-config.module.ts
+++ src/app-config.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from 'config/configuration';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { RoleModule } from './modules/role/role.module';
+
+const SYSTEM_MODULES = [UserModule, RoleModule];
 
 @Module({
   imports: [
@@ -11,8 +14,7 @@ import { AuthModule } from './modules/auth/auth.module';
       load: [configuration],
       isGlobal: true,
     }),
-    UserModule,
-    AuthModule,
+    ...SYSTEM_MODULES,
   ],
   exports: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
+import { GlobalExceptionFilter } from './utils/handler/exception.handler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +11,8 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
     credentials: true,
   });
-  app.use(cookieParser())
+  app.use(cookieParser());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(configService.get<string>('http.port'));
 }
 bootstrap();
