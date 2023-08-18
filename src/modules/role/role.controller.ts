ca4dfef0e90b1eefaf2401d82be8101c6c6909ba
tutn <azjustin3@gmail.com>
2023-08-18 18:16:36 +0700

    Update: add role CRUD

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { RoleService } from './role.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put
+} from '@nestjs/common';
 import { RoleDTO } from './dto/role.dto';
+import { RoleService } from './role.service';
 
 @Controller('rest/role')
 export class RoleController {
@@ -14,5 +22,15 @@ export class RoleController {
   @Post()
   createNewRole(@Body() roleDTO: RoleDTO) {
     return this.roleService.createNew(roleDTO);
+  }
+
+  @Put(':id')
+  updateRole(@Param('id') id: number, @Body() roleDTO: RoleDTO) {
+    return this.roleService.updateById(id, roleDTO);
+  }
+
+  @Delete(':id')
+  deleteRole(@Param('id') id: number) {
+    return this.roleService.deleteById(id);
   }
 }
